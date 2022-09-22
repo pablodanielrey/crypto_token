59b45f221a258da43992af8b6857e07e8bbfe706
Pablo Daniel Rey <pablodanielrey@gmail.com>
2022-09-21 22:27:04 -0300

    added more dumping info

--- ver_atributos_token.py
+++ ver_atributos_token.py
@@ -4,19 +4,21 @@ import pkcs11
 from pkcs11 import KeyType, ObjectClass, Mechanism
 
 
-user_pin = os.environ.get('pkcs11_pin')
+user_pin = os.environ.get("pkcs11_pin")
 if not user_pin:
     sys.exit(1)
 
 p = "/opt/CryptoIDEUser/x64/lib/linux/x64/libcryptoide_pkcs11.so"
 cry = pkcs11.lib(p)
-print('libreria inicializada')
+print("libreria inicializada")
 for slot in cry.get_slots(token_present=True):
-    print(f'slot encontrado : {slot}')
+    print(f"slot encontrado : {slot}")
     token = slot.get_token()
     print(token)
     print(token.label)
     print(token.flags)
+    print(token.serial)
     with token.open(user_pin=user_pin) as session:
-        print('Se obtuvo una sesión correctamente')
-        
+        print("Se obtuvo una sesión correctamente")
+        for o in session.get_objects():
+            print(o)
